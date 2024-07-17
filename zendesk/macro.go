76273ff135a7a8dc744295a73809c48a59fb13f1
Potter, Jacob <Jacob.Potter@dynatrace.com>
2024-07-17 05:39:24 -0400

    fix: return restriction to interface

--- zendesk/macro.go
+++ zendesk/macro.go
@@ -9,16 +9,16 @@ import (
 
 // Macro is information about zendesk macro
 type Macro struct {
-	Actions     []MacroAction     `json:"actions"`
-	Active      bool              `json:"active"`
-	CreatedAt   time.Time         `json:"created_at,omitempty"`
-	Description string            `json:"description,omitempty"`
-	ID          int64             `json:"id,omitempty"`
-	Position    int               `json:"position,omitempty"`
-	Restriction *MacroRestriction `json:"restriction,omitempty"`
-	Title       string            `json:"title"`
-	UpdatedAt   time.Time         `json:"updated_at,omitempty"`
-	URL         string            `json:"url,omitempty"`
+	Actions     []MacroAction `json:"actions"`
+	Active      bool          `json:"active"`
+	CreatedAt   time.Time     `json:"created_at,omitempty"`
+	Description string        `json:"description,omitempty"`
+	ID          int64         `json:"id,omitempty"`
+	Position    int           `json:"position,omitempty"`
+	Restriction interface{}   `json:"restriction,omitempty"`
+	Title       string        `json:"title"`
+	UpdatedAt   time.Time     `json:"updated_at,omitempty"`
+	URL         string        `json:"url,omitempty"`
 }
 
 // MacroAction is definition of what the macro does to the ticket

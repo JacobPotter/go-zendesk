b7d857f56e24c80fe0fbfc12a6364c794e742c55
Potter, Jacob <Jacob.Potter@dynatrace.com>
2024-07-29 23:46:11 -0400

    fix: update ticket form types and add basic validation

--- zendesk/ticket_form.go
+++ zendesk/ticket_form.go
@@ -4,23 +4,107 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"slices"
+	"time"
 )
 
-// TicketForm is JSON payload struct
+// TicketForm ticket forms allow an admin to define a subset of ticket fields for display to both agents and end users.
+// Accounts are limited to 300 ticket forms.
+//
+// TicketFieldIds is used to display all ticket fields which are in this ticket form.
+// The products use the order of the ids to show the field values in the tickets.
+//
+// AgentConditions and EndUserConditions specify which ticket fields to show on the form based on a set of defined conditions
 type TicketForm struct {
-	ID                 int64   `json:"id,omitempty"`
-	URL                string  `json:"url,omitempty"`
-	Name               string  `json:"name"`
-	RawName            string  `json:"raw_name,omitempty"`
-	DisplayName        string  `json:"display_name,omitempty"`
-	RawDisplayName     string  `json:"raw_display_name,omitempty"`
-	Position           int64   `json:"position"`
-	Active             bool    `json:"active,omitempty"`
-	EndUserVisible     bool    `json:"end_user_visible,omitempty"`
-	Default            bool    `json:"default,omitempty"`
-	TicketFieldIDs     []int64 `json:"ticket_field_ids,omitempty"`
-	InAllBrands        bool    `json:"in_all_brands,omitempty"`
-	RestrictedBrandIDs []int64 `json:"restricted_brand_ids,omitempty"`
+	Active             bool                     `json:"active"`
+	AgentConditions    []ConditionalTicketField `json:"agent_conditions"`
+	CreatedAt          time.Time                `json:"created_at"`
+	Default            bool                     `json:"default"`
+	DisplayName        string                   `json:"display_name"`
+	EndUserConditions  []ConditionalTicketField `json:"end_user_conditions"`
+	EndUserVisible     bool                     `json:"end_user_visible"`
+	ID                 int64                    `json:"id"`
+	InAllBrands        bool                     `json:"in_all_brands"`
+	Name               string                   `json:"name"`
+	Position           int64                    `json:"position"`
+	RawDisplayName     string                   `json:"raw_display_name"`
+	RawName            string                   `json:"raw_name"`
+	RestrictedBrandIds []int64                  `json:"restricted_brand_ids"`
+	TicketFieldIds     []int64                  `json:"ticket_field_ids"`
+	UpdatedAt          time.Time                `json:"updated_at"`
+	Url                string                   `json:"url"`
+}
+
+// ConditionalTicketField condition which to display fields ParentFieldId is the ticket field the condition is for,
+// where the condition is matching the value in Value.
+// The Value will either be the tag value of a field or a case-sensitive match of a text field.
+// Then, ChildFields is the set of fields to show when the condition is met.
+// There is then a set of statuses to be required on, if it is required
+type ConditionalTicketField struct {
+	ParentFieldId int64        `json:"parent_field_id"`
+	Value         string       `json:"value"`
+	ChildFields   []ChildField `json:"child_fields"`
+}
+
+// ChildField is used to define when to show a field based on a condition from a parent field in a form.
+// Has attributes IsRequired and RequiredOnStatuses, which indicate if the field to show is conditional
+type ChildField struct {
+	Id                 int64              `json:"id"`
+	IsRequired         bool               `json:"is_required"`
+	RequiredOnStatuses RequiredOnStatuses `json:"required_on_statuses"`
+}
+
+// RequiredOnStatuses is an object that defines how status requires a child field on a form.
+// Valid types in Type attribute are ALL_STATUSES, NO_STATUSES, and SOME_STATUSES.
+// Statuses will enclose the statuses the field is required when Type is SOME_STATUSES.
+// Otherwise, it is empty
+type RequiredOnStatuses struct {
+	Statuses []string        `json:"statuses"`
+	Type     RequirementType `json:"type"`
+}
+
+// Validate validates attributes in RequiredOnStatuses struct. Returns an error if validation fails
+func (r RequiredOnStatuses) Validate() error {
+	switch r.Type {
+	case AllStatuses, NoStatuses:
+		if len(r.Statuses) > 0 {
+			return fmt.Errorf("error: no statuses when type is ALL_STATUSES or NO_STATUSES")
+		}
+	case SomeStatuses:
+		if len(r.Statuses) == 0 {
+			return fmt.Errorf("error: statuses required when type is SOME_STATUSES")
+		}
+		for _, status := range r.Statuses {
+			if !slices.Contains(ValidRequirementStatuses, status) {
+				return fmt.Errorf("error: status '%s' is not valid", status)
+			}
+		}
+	}
+	return nil
+}
+
+// ValidRequirementStatuses is an array of strings denoting which statuses are available to be used in RequiredOnStatuses struct
+var ValidRequirementStatuses = []string{"new", "open", "pending", "hold", "solved"}
+
+type RequirementType string
+
+const (
+	SomeStatuses RequirementType = "SOME_STATUSES"
+	NoStatuses   RequirementType = "NO_STATUSES"
+	AllStatuses  RequirementType = "ALL_STATUSES"
+)
+
+// ValidRequirementTypes denotes which status requirement types are valid
+var ValidRequirementTypes = []RequirementType{
+	SomeStatuses, AllStatuses, NoStatuses,
+}
+
+// Validate validates status requirement types
+func (r RequirementType) Validate() error {
+	if !slices.Contains(ValidRequirementTypes, r) {
+		return fmt.Errorf("requirement type '%s' is not valid", r)
+	}
+	return nil
 }
 
 // TicketFormListOptions is options for GetTicketForms

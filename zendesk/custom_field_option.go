56a0044daf5cf1336e2e404b9bd860302bfc7b84
Potter, Jacob <Jacob.Potter@dynatrace.com>
2024-11-20 09:23:10 -0500

    fix: cfo id needs to be pointer so nil value gets converted to json NULL

--- zendesk/custom_field_option.go
+++ zendesk/custom_field_option.go
@@ -2,7 +2,7 @@ package zendesk
 
 // CustomFieldOption is struct for value of `custom_field_options`
 type CustomFieldOption struct {
-	ID       int64  `json:"id,omitempty"`
+	ID       *int64 `json:"id"`
 	Name     string `json:"name"`
 	Position int64  `json:"position,omitempty"`
 	RawName  string `json:"raw_name,omitempty"`
